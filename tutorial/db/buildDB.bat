b84e738e50e85d7bb694f3b85abf2df12a441ec8
Jayman Gandhi <jayman.gandhi@gmail.com>
2016-12-30 06:49:02 -0500

    updated database location for roundhouse

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -1,7 +1,7 @@
 @echo off
 SET DIR=%~d0%~p0%
 
-SET connstring="data source=DESKTOP-3109F0E\SQLEXPRESS;initial catalog=Northwind;integrated security=True"
+SET connstring="data source=LOCALHOST\SQLEXPRESS;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/gandhij2016/tutorial-demo-net"
  
 "%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
